7a05f648d03b44505aafc7872a400bc1d0d3d77c
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-02-28 14:29:32 -0500

    test: optionally ignore requests_mock logging

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,29 @@
 """Provide core testing utilities."""
 
+import logging
 import shutil
 from pathlib import Path
 
 import pytest
+
+
+def pytest_addoption(parser):
+    """Add custom commands to pytest invocation.
+
+    See https://docs.pytest.org/en/8.1.x/reference/reference.html#parser
+    """
+    parser.addoption(
+        "--verbose-logs",
+        action="store_true",
+        default=False,
+        help="show noisy module logs",
+    )
+
+
+def pytest_configure(config):
+    """Configure pytest setup."""
+    if not config.getoption("--verbose-logs"):
+        logging.getLogger("requests_mock.adapter").setLevel(logging.INFO)
 
 
 @pytest.fixture(scope="session")
