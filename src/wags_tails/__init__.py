64b04a15ba5a1709bf5e60ab3b2ebb27f62d300b
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-03-08 09:04:24 -0500

    feat: add HPO support (#71)

--- src/wags_tails/__init__.py
+++ src/wags_tails/__init__.py
@@ -2,26 +2,27 @@
 
 from importlib.metadata import PackageNotFoundError, version
 
-from .base_source import DataSource, RemoteDataError
-from .chembl import ChemblData
-from .chemidplus import ChemIDplusData
-from .custom import CustomData
-from .do import DoData
-from .drugbank import DrugBankData
-from .drugsatfda import DrugsAtFdaData
-from .ensembl import EnsemblData
-from .ensembl_transcript_mappings import EnsemblTranscriptMappingData
-from .guide_to_pharmacology import GToPLigandData
-from .hemonc import HemOncData
-from .hgnc import HgncData
-from .moa import MoaData
-from .mondo import MondoData
-from .ncbi import NcbiGeneData, NcbiGenomeData
-from .ncbi_lrg_refseqgene import NcbiLrgRefSeqGeneData
-from .ncbi_mane_summary import NcbiManeSummaryData
-from .ncit import NcitData
-from .oncotree import OncoTreeData
-from .rxnorm import RxNormData
+from wags_tails.base_source import DataSource, RemoteDataError
+from wags_tails.chembl import ChemblData
+from wags_tails.chemidplus import ChemIDplusData
+from wags_tails.custom import CustomData
+from wags_tails.do import DoData
+from wags_tails.drugbank import DrugBankData
+from wags_tails.drugsatfda import DrugsAtFdaData
+from wags_tails.ensembl import EnsemblData
+from wags_tails.ensembl_transcript_mappings import EnsemblTranscriptMappingData
+from wags_tails.guide_to_pharmacology import GToPLigandData
+from wags_tails.hemonc import HemOncData
+from wags_tails.hgnc import HgncData
+from wags_tails.hpo import HpoData
+from wags_tails.moa import MoaData
+from wags_tails.mondo import MondoData
+from wags_tails.ncbi import NcbiGeneData, NcbiGenomeData
+from wags_tails.ncbi_lrg_refseqgene import NcbiLrgRefSeqGeneData
+from wags_tails.ncbi_mane_summary import NcbiManeSummaryData
+from wags_tails.ncit import NcitData
+from wags_tails.oncotree import OncoTreeData
+from wags_tails.rxnorm import RxNormData
 
 try:
     __version__ = version("wags-tails")
@@ -43,6 +44,7 @@ __all__ = [
     "GToPLigandData",
     "HemOncData",
     "HgncData",
+    "HpoData",
     "MoaData",
     "MondoData",
     "NcbiGeneData",
