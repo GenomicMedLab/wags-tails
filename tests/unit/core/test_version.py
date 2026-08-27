e92daa99258eb29040ba6a7d186fce4f6e6943a5
James Stevenson <james.stevenson@nationwidechildrens.org>
2026-08-27 11:01:37 -0400

    add initial tests

---
+++ tests/unit/core/test_version.py
@@ -0,0 +1,59 @@
+from datetime import date
+
+import pytest
+
+from wags_tails.core.version import (
+    DashSeparatedVersionScheme,
+    DateVersionScheme,
+    DotSeparatedVersionScheme,
+    IntegerVersionScheme,
+    Version,
+)
+
+
+@pytest.mark.parametrize(
+    ("value", "expected"),
+    [
+        ("36", 36),
+        ("v30", 30),
+        ("v.25", 25),
+    ],
+)
+def test_int_version_scheme(value, expected):
+    assert Version.parse(value, IntegerVersionScheme).parsed == expected
+
+
+@pytest.mark.parametrize(
+    ("value", "expected"),
+    [
+        ("2026-08-26", date(2026, 8, 26)),
+        ("v2026-08-26", date(2026, 8, 26)),
+        ("v.2026-08-26", date(2026, 8, 26)),
+    ],
+)
+def test_date_version_scheme(value, expected):
+    assert Version.parse(value, DateVersionScheme).parsed == expected
+
+
+@pytest.mark.parametrize(
+    ("value", "expected"),
+    [
+        ("5.4.1", (5, 4, 1)),
+        ("v2.0", (2, 0)),
+        ("v.5.1.6", (5, 1, 6)),
+    ],
+)
+def test_dot_version_scheme(value, expected):
+    assert Version.parse(value, DotSeparatedVersionScheme).parsed == expected
+
+
+@pytest.mark.parametrize(
+    ("value", "expected"),
+    [
+        ("5-4-1", (5, 4, 1)),
+        ("v2-0", (2, 0)),
+        ("v.5-1-6", (5, 1, 6)),
+    ],
+)
+def test_dash_version_scheme(value, expected):
+    assert Version.parse(value, DashSeparatedVersionScheme).parsed == expected
